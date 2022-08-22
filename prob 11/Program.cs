b42a7fb54913854fcdfe8d35483bc7dcf44d8fce
username <username@cognizant.com>
2022-08-22 21:11:58 +0530

    Add project files.

---
+++ prob 11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<int, string> dic = new Dictionary<int, string>();
+            //dic.Add(1, "Delhi");
+            //dic.Add(2, "Mumbai");
+            //dic.Add(3, "Chennai");
+
+            //foreach (KeyValuePair<int, string> item in dic)
+            //{
+            //    Console.WriteLine(item.Key + " " + item.Value);
+
+            //}
+
+            SortedList s1 = new SortedList();
+            s1.Add(1, "Delhi");
+            s1.Add(2, "Mumbai");
+            s1.Add(3, "Chennai");
+            foreach (DictionaryEntry item in s1)
+            {
+                Console.WriteLine(item.Key + "  " + item.Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
